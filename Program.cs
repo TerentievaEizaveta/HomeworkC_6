4a54b950323128b228e6a3c6e5dbf358142ec8a9
User <elizaveta.terentieva1993@yandex.ru>
2022-12-19 15:58:48 +0300

    Added second task

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// Домашняя работа
 /*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
 ввёл пользователь.*/
+/*
 Console.Clear();
 void NumberOfPositive(int size)
 {
@@ -19,7 +20,27 @@ void NumberOfPositive(int size)
 Console.Write("Input number of numbers: ");
 int M = Convert.ToInt32(Console.ReadLine());
 NumberOfPositive(M);
-
+*/
 /* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 значения b1, k1, b2 и k2 задаются пользователем.*/
+/*
+void Point(int b1, int k1, int b2, int k2)
+{
+    double x =(double) (b2-b1)/(k1-k2);
+    double y = (double)(k1*x+b1);
+    if ( k1==k2) Console.WriteLine($"Lines are parallel.");
+    else if ( (b1==b2)&& ( k1==k2) ) Console.WriteLine($"Lines will match.");
+    else 
+        Console.WriteLine($"Intersection lines y={k1}x+{b1} and y={k2}x+{b2} is a point ({x};{y}).");
+}
+Console.Write("Input b1 ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k1 ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2 ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k2 ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Point(b1,k1,b2,k2);
+*/
